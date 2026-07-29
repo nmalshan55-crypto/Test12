b378989af9b8ac9e795b31ac8953553b1167dbe1
nmalshan55-crypto <nmalshan55@gmail.com>
2026-07-29 18:38:00 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -2,7 +2,7 @@ module.exports = {
     cmd: 'alive',
     desc: 'Check bot online status',
     handler: async (sock, msg, from, args, { BOT_NAME }) => {
-        const imageUrl = 'https://i.ibb.co/689N3M4/alive-image.jpg'; // ඔයාට ඕන Image Link එකක් මෙතනට දාන්න පුළුවන්
+        const imageUrl = '<a href="https://ibb.co/9mHXDL2n"><img src="https://i.ibb.co/7xtcf5Vv/file-0000000002d48230a5ad48cf94c182d7.png" alt="file-0000000002d48230a5ad48cf94c182d7" border="0"></a>'; // ඔයාට ඕන Image Link එකක් මෙතනට දාන්න පුළුවන්
 
         const aliveText = `*───────────────────*\n` +
                           `🎉 *${BOT_NAME} CONNECTED!* 🟢\n` +
